d94859f94469923174558411b0d0559989bc421d
Diego Camacho <hackvan@gmail.com>
2018-07-10 14:07:43 -0500

    Creating modules for use as instance and classes methods

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,13 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid
+  include Dance # use all of the methods of the included module as instance methods. 
+  extend  MetaDancing # use all of the methods of the extending module as class methods.
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
