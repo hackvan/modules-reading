cf269355f443b834c40f90c34cabaac7676884f8
Diego Camacho <hackvan@gmail.com>
2018-07-10 14:19:05 -0500

    refactor the two modules into one FancyDance

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,9 +1,10 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
 
 class Kid
-  include Dance # use all of the methods of the included module as instance methods. 
-  extend  MetaDancing # use all of the methods of the extending module as class methods.
+  extend  FancyDance::ClassMethods    # use all of the methods of the extending module as class methods.
+  include FancyDance::InstanceMethods # use all of the methods of the included module as instance methods. 
 
   attr_accessor :name
 
